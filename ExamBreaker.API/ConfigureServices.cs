6d72e2f0df74fa613a4b5dc30ef553bd1c48bdcf
YuraSoroka <72869941+YuraSoroka@users.noreply.github.com>
2024-04-06 19:24:02 +0300

    Configure fast endpoints (#2)
    * Fast Endpoints library configuration + test endpoint
    
    * Removed redundant code

---
+++ ExamBreaker.API/ConfigureServices.cs
@@ -0,0 +1,16 @@
+﻿using FastEndpoints;
+using FastEndpoints.Swagger;
+
+namespace ExamBreaker.API;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection AddWebApiServices(this IServiceCollection services)
+    {
+        services
+            .AddFastEndpoints()
+            .SwaggerDocument();
+
+        return services;
+    }
+}
---
+++ ExamBreaker.API/Endpoints/Hotspots/HotspotByIdEndpoint.cs
@@ -0,0 +1,17 @@
+﻿using FastEndpoints;
+
+namespace ExamBreaker.API.Endpoints.Hotspots;
+
+public class HotspotByIdEndpoint : Endpoint<HotspotByIdRequest, string>
+{
+    public override void Configure()
+    {
+        Get("api/hotspots/{id}");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(HotspotByIdRequest req, CancellationToken ct)
+    {
+        await SendAsync("Success", cancellation: ct);
+    }
+}
