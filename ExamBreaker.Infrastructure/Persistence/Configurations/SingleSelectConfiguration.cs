019d3b0506e5a024da0ad7f61d4ae12ca47300d5
Yurii Soroka <yurasoroka1999@gmail.com>
2024-04-13 20:52:35 +0300

    Added configuration for SingleSelect Aggregate root

--- ExamBreaker.Infrastructure/Persistence/Configurations/SingleSelectConfiguration.cs
+++ ExamBreaker.Infrastructure/Persistence/Configurations/SingleSelectConfiguration.cs
@@ -1,13 +1,50 @@
 ﻿using ExamBreaker.Domain.Agggregates.SingleSelects;
+using ExamBreaker.Domain.Agggregates.SingleSelects.ValueObjects;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace ExamBreaker.Infrastructure.Persistence.Configurations;
 
-internal class SingleSelectConfiguration : IEntityTypeConfiguration<SingleSelectQuestion>
+internal sealed class SingleSelectConfiguration : IEntityTypeConfiguration<SingleSelectQuestion>
 {
     public void Configure(EntityTypeBuilder<SingleSelectQuestion> builder)
     {
+        ConfigureSingleSelectQuestionTable(builder);
+        ConfigureQuestionOptionsTable(builder);
+    }
+
+    private void ConfigureSingleSelectQuestionTable(EntityTypeBuilder<SingleSelectQuestion> builder)
+    {
+        builder.ToTable("SingleSelectQuestions");
+
         builder.HasKey(x => x.Id);
+
+        builder
+            .Property(x => x.Id)
+            .ValueGeneratedNever()
+            .HasConversion(
+                id => id.Value,
+                value => SingleSelectId.Create(value));
+    }
+
+    private void ConfigureQuestionOptionsTable(EntityTypeBuilder<SingleSelectQuestion> builder)
+    {
+
+        builder.OwnsMany(q => q.Options, optionsBuilder =>
+        {
+            optionsBuilder.ToTable("SingleSelectOptions");
+
+            optionsBuilder
+                .WithOwner()
+                .HasForeignKey("QuestionId");
+
+            optionsBuilder.HasKey(x => x.Id);
+            optionsBuilder
+                .Property(x => x.Id)
+                .ValueGeneratedNever()
+                .HasConversion(
+                    id => id.Value,
+                    value => QuestionOptionId.Create(value));
+        });
     }
 }
